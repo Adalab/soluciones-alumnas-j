7e5878b56c7e8057150da8b84a9efaeaeb3dd854
Mercedes <mcarballalfr@gmail.com>
2020-07-15 08:33:08 +0200

    Modulo 3, Leccion 1, Ejercicios

---
+++ MODULO-3/Leccion-01/modulo-03-leccion-01-ejercicio-03-Mercedes/dog.js
@@ -0,0 +1,10 @@
+'use strict';
+class Dog {
+  bark() {
+    console.log('Wan, wan!');
+  }
+}
+
+const FAMOUS_DOGS = ['Hachiko', 'Laika', '101 Dalmatians'];
+
+export { Dog, FAMOUS_DOGS }; //Exportamos la clase y la constante
